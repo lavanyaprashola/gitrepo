1a39fcfc86c8a2df5d832868751b35928d89b1ff
lavanya eswaran <96522510+lavanyaprashola@users.noreply.github.com>
2022-04-06 20:16:11 +0530

    Add files via upload

---
+++ ec2-module/ec2.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+  region = var.region
+}
+
+# data "aws_subnet" "subnet" {
+
+#   filter {
+#     name   = "tag:Name"
+#     values = ["pub-*"]
+#   }
+# }
+
+# data "aws_vpc" "test-vpc" {
+
+#     filter {
+#       name   = "tag:Name"
+#       values = ["test-*"]
+#     }
+# }
+
+resource "aws_security_group" "test-sg" {
+  name        = "test-sgp"
+  description = "Security group"
+  vpc_id      = "vpc-0ab8b6c6af5b47818"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "test-sgp"
+  }
+}
+
+
+
+resource "aws_instance" "test-ins" {
+  ami = var.ami
+  instance_type = var.instance_type
+  key_name = var.key_name
+  subnet_id = "subnet-06ad25b11c3a9f105"
+}
---
+++ ec2-module/terraform.tfvars
@@ -0,0 +1,7 @@
+region = "us-west-2"
+
+ami = "ami-0eb324d928acca58a"
+
+key_name = "mickeymouse"
+
+instance_type = "t2.micro"
