f79090ad2d82f0756e1843ed66ba0fe753d08723
lavanya eswaran <96522510+lavanyaprashola@users.noreply.github.com>
2022-04-06 20:17:48 +0530

    Delete ec2-module directory

--- ec2-module/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "myprashola"
-    region = "us-east-1"
-    key = "ec2/terraform.tfstate"
-  }
-}
--- ec2-module/terraform.tfvars
+++
@@ -1,7 +0,0 @@
-region = "us-west-2"
-
-ami = "ami-0eb324d928acca58a"
-
-key_name = "mickeymouse"
-
-instance_type = "t2.micro"
